96d66b48cf8d6846bbc0a5927f7810a19fc1a288
Wiston Altamirano <wistonam@hotmail.com>
2016-05-01 13:36:20 -0600

    removiendo los menus que no se utilizaran

--- application/views/administracion/themes/default/header.php
+++ application/views/administracion/themes/default/header.php
@@ -81,29 +81,7 @@ $usuario = $this->session->userdata('usuario');
                     <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
                 </a>
                 <ul class="dropdown-menu dropdown-messages">
-                    <li>
-                        <a href="#">
-                            <div>
-                                <strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                            </div>
-                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                        </a>
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <a href="#">
-                            <div>
-                                <strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                            </div>
-                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                        </a>
-                    </li>
+
                     <li class="divider"></li>
                     <li>
                         <a href="#">
